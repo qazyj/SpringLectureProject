13c6e4925ba50e2d5e509f73978d80bf10dd995b
qazyj <qazyj@naver.com>
2021-12-22 22:39:43 +0900

    [ADD/기본편] AppConfig 객체를 추가하여 개방-폐쇄, 의존관게 역전 원칙을 지킴

--- 기본편/core/src/test/java/hello/core/member/memberServiceTest.java
+++ 기본편/core/src/test/java/hello/core/member/memberServiceTest.java
@@ -1,10 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class memberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appconfig = new AppConfig();
+        memberService = appconfig.memberService();
+    }
+
 
     @Test
     void join() {
