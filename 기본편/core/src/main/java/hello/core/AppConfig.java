c6046ddc93d4f511f3e075a803cd8b4a8419171b
qazyj <qazyj@naver.com>
2021-12-23 00:18:35 +0900

    [UPDATE/기본편] AppConfig spring으로 변환

--- 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,40 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FIxDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration          // 설정 정보에 하는 어노테이션
 public class AppConfig {
 
+    @Bean           // 각 메서드에 Bean 어노테이션을 추가하면 스프링 컨테이너에 추가됨.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
+    @Bean
+    public MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FIxDiscountPolicy());
+                getMemberRepository(),
+                getDigcountPolicy());
     }
 
+    @Bean
+    public DiscountPolicy getDigcountPolicy() {
+        //return new FIxDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
 }
