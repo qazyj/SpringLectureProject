601a7b74a824545ff0c26b0a78ad1ac4460122ea
qazyj <qazyj@naver.com>
2021-12-23 20:56:08 +0900

    [ADD/기본편] singleton, singleton test

--- 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -28,4 +28,26 @@ public class SingletonTest {
         // memberService != memberService1
         assertThat(memberService).isNotSameAs(memberService1);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다.
+        // new SingletonService();
+
+        // 1. 조회 : 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService = SingletonService.getInstance();
+
+        // 2. 조회 : 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+
+        // 참조값이 같은 것을 확인
+        System.out.println("singletonService1 : " + singletonService);
+        System.out.println("singletonService2 : " + singletonService1);
+
+        // singletonService1 == singletonService2
+        assertThat(singletonService).isSameAs(singletonService1);
+
+        singletonService1.logic();
+    }
 }
