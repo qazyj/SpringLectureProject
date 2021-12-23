a9c4eead9b3bd58cec608af49073e5f9f61b5f97
qazyj <qazyj@naver.com>
2021-12-23 19:48:12 +0900

    [ADD/기본편] 객체 생성 확인

---
+++ 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,31 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService = appConfig.memberService();
+
+        // 2. 조회 : 조회할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 : " + memberService);
+        System.out.println("memberService2 : " + memberService1);
+
+        // memberService != memberService1
+        assertThat(memberService).isNotSameAs(memberService1);
+    }
+}
