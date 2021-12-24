f6c7890c0706be33db3d8e08de39800fc7b4844c
qazyj <qazyj@naver.com>
2021-12-24 13:08:21 +0900

    [ADD/기본편] Spring Bean Singleton Test

--- 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,5 +51,24 @@ public class SingletonTest {
         assertThat(singletonService).isSameAs(singletonService1);
 
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때 마다 같은 객체를 반환
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        // 2. 조회 : 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 : " + memberService);
+        System.out.println("memberService2 : " + memberService1);
+
+        // memberService == memberService1
+        assertThat(memberService).isSameAs(memberService1);
     }
 }
