9c60d689128132285ebd028d8a7c8128b5c11b42
qazyj <qazyj@naver.com>
2022-03-08 01:42:03 +0900

    엔티티 관련 테스트

---
+++ jpa/basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,50 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member1 = new Member();
+            member1.setUsername("A");
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("===============");
+
+            // DB SEQ = 1     |     1
+            // DB SEQ = 51    |     2
+            // DB SEQ = 51    |     3
+
+            em.persist(member1);    // 1, 51
+            em.persist(member2);    // MEM
+            em.persist(member3);    // MEM
+
+            System.out.println("member.id = " + member1.getId());
+            System.out.println("member.id = " + member2.getId());
+            System.out.println("member.id = " + member3.getId());
+            System.out.println("===============");
+
+            tx.commit();
+            } catch(Exception e){
+                tx.rollback();
+            } finally{
+                em.close();
+                emf.close();
+            }
+        }
+
+}
