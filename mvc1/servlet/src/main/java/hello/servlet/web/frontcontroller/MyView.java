58505256b640ff6003bda0a2f31e245492c45abc
qazyj <qazyj@naver.com>
2022-06-03 20:41:13 +0900

    Revert "[ADD] V3 - model, uri 메소드화 (front controller가 복잡해짐)"
    This reverts commit 63bb88027ebd4f8addb96327fe5a9724112c51dc.

--- mvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ mvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -15,12 +14,6 @@ public class MyView {
     }
 
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request, response);
-    }
-
-    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        model.forEach((key, value) -> request.setAttribute(key, value));
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
